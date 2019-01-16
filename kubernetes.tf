5c0bea489960deb3c787925f5a76cd19ac587941
karakyrt <44345658+karakyrt@users.noreply.github.com>
2019-01-16 16:09:32 -0600

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -772,7 +772,7 @@ resource "aws_route53_record" "api-tarantul-qa-net" {
     evaluate_target_health = false
   }
 
-  zone_id = "/hostedzone/Z143SG7LT37JNZ"
+  zone_id = "/hostedzone/Z37FW6LNTWKG2C"
 }
 
 resource "aws_route53_record" "bastion-tarantul-qa-net" {
@@ -785,11 +785,11 @@ resource "aws_route53_record" "bastion-tarantul-qa-net" {
     evaluate_target_health = false
   }
 
-  zone_id = "/hostedzone/Z143SG7LT37JNZ"
+  zone_id = "/hostedzone/Z37FW6LNTWKG2C"
 }
 
-resource "aws_route53_zone_association" "Z143SG7LT37JNZ" {
-  zone_id = "/hostedzone/Z143SG7LT37JNZ"
+resource "aws_route53_zone_association" "Z37FW6LNTWKG2C" {
+  zone_id = "/hostedzone/Z37FW6LNTWKG2C"
   vpc_id  = "${aws_vpc.tarantul-qa-net.id}"
 }
 
